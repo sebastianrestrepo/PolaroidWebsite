b339858512be3968c58726d8873ed6728c1b2c3f
Sebastián Restrepo <sebastianrestrepoq@gmail.com>
2018-03-23 01:44:30 -0500

    paddings

--- main.js
+++ main.js
@@ -27,19 +27,19 @@ function showDivs(n) {
     switch (slideIndex) {
         case 1:
         document.getElementById("titleinfo").innerHTML = "Camera One Step 2";
-        document.getElementById("pinfo").innerHTML = "The OneStep 2 is the new polaroid camera that blends classic design with <br> contemporary style to bring you the best experience.";
+        document.getElementById("pinfo").innerHTML = "The OneStep 2 is the new polaroid camera that blends classic design with contemporary style to bring you the best experience.";
             break;
         case 2:
         document.getElementById("titleinfo").innerHTML = "Polaroid Snap";
-        document.getElementById("pinfo").innerHTML = "The Polaroid Snap is the perfect blend of nostalgic Polaroid instant <br> photography and modern, digital technology for you.";
+        document.getElementById("pinfo").innerHTML = "The Polaroid Snap is the perfect blend of nostalgic Polaroid instant photography and modern, digital technology for you.";
             break;
         case 3:
         document.getElementById("titleinfo").innerHTML = "Polaroid z2300";
-        document.getElementById("pinfo").innerHTML = "The z2300 is the new polaroid camera that blends classic design with <br> contemporary style to bring you the best experience.";
+        document.getElementById("pinfo").innerHTML = "The z2300 is the new polaroid camera that blends classic design with contemporary style to bring you the best experience.";
             break;
         case 4:
         document.getElementById("titleinfo").innerHTML = "Polaroid Cube";
-        document.getElementById("pinfo").innerHTML = "Now with Wi-Fi, the new Polaroid Cube+ lifestyle action camera makes <br> sharing your favorite photos and videos even easier in history ";
+        document.getElementById("pinfo").innerHTML = "Now with Wi-Fi, the new Polaroid Cube+ lifestyle action camera makes sharing your favorite photos and videos even easier in history ";
             break;
     }
 
